92c5297d196c620672b03d4fdc0c2ec174014821
ricardaluther <anluther@uni-osnabrueck.de>
2021-08-23 11:56:18 +0200

    Suggestion for player handling

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    private int _speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0f, 0.5f, -1.5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxis("Mouse X") > 0)
+        {
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * _speed,
+                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * _speed);
+        }
+
+        else if (Input.GetAxis("Mouse X") < 0)
+        {
+            transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * _speed,
+                                       0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * _speed);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Resources;
+using UnityEditor;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _speed = 5f;
+    
+    [Header("Player Settings")]
+    [SerializeField]
+    
+    private int _lives = 3;
+
+    public static Vector3 playerPos;
+
+
+
+    void Start()
+    {
+        transform.position = new Vector3(0f, 0f, 0f);
+    }
+   
+    void Update()
+    {
+        PlayerMovement();
+    }
+    void PlayerMovement()
+    {
+     float horizontalInput = Input.GetAxis("Horizontal");
+     float verticalInput = Input.GetAxis("Vertical");
+     Vector3 playerTranslate = new Vector3(1f * horizontalInput * _speed * Time.deltaTime,
+               0f,
+               1f * verticalInput * _speed * Time.deltaTime);
+               transform.Translate(playerTranslate);
+    }
+}
