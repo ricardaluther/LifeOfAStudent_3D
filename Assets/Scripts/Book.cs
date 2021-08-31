b6f833fac94812f0e6d837cdc17b1f98db11d35b
Carina <clkoenig96@web.de>
2021-08-31 20:19:16 +0200

    LocationChange fixed

--- Assets/Scripts/Book.cs
+++ Assets/Scripts/Book.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Book : MonoBehaviour
 {
+    private Vector3 startingPosition;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        startingPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private Vector3 GetRoamingPosition()
+    {
+        return startingPosition + GetRandomDir() * Random.Range(10f, 70f);
+    }
+
+    //returns a random Vector3
+    public static Vector3 GetRandomDir()
+    {
+        return new Vector3(UnityEngine.Random.Range(-1f, 1f), 0.88f, UnityEngine.Random.Range(-1f, 1f)).normalized;
     }
 }
